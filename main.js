22f4f259e363c862c1ce29d7f7f3a42a18f418b8
Jonathan Jacka <jonathanjacka@gmail.com>
2022-01-26 13:24:35 -0600

    Updated letter entry, check and set word methods

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
-console.log('Hello World!');
+const state = {
+    word: 'audio',
+    attempt: 0,
+    isSolved: false,
+    currentWord: []
+}
+
+checkRowComplete = (row) => {
+    let count = 0;
+    row.forEach(cell => cell.value !== '' && count++);
+    return count === 5 ? true : false;
+}
+
+const updateCurrentWord = (row) => {
+    const word = []; 
+    row.forEach(cell => word.push(cell.value));
+    return word;
+}
+
+const row = document.querySelectorAll('.cell');
+row[0].focus();
+
+row.forEach((code, idx) => {
+  code.addEventListener('keydown', (event) => {
+    if (event.key >= 'a' && event.key <= 'z') {
+      row[idx].value = '';
+
+      if(idx < row.length - 1) {
+        setTimeout(() => row[idx + 1].focus(), 10);
+      }
+      
+    } else if (event.key === 'Backspace' && idx !== 0) {
+      setTimeout(() => row[idx - 1].focus(), 10);
+    } 
+  });
+});
+
+document.addEventListener('keydown',(event) => {
+    if(event.key === 'Enter' || event.key === 'Return') {
+        console.log('Enter was pressed!');
+        console.log(checkRowComplete(row));
+        console.log(updateCurrentWord(row));
+    }
+});
+
