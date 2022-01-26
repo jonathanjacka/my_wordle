fbe5fa28d6a3f984e4938979ac4c6975b3eafcb7
Jonathan Jacka <jonathanjacka@gmail.com>
2022-01-26 16:09:13 -0600

    Updated key entry

--- main.js
+++ main.js
@@ -1,45 +1,91 @@
 const state = {
     word: 'audio',
-    attempt: 0,
+    attempts: 0,
     isSolved: false,
+    currentRow: document.getElementById('row-1'),
     currentWord: []
 }
 
-checkRowComplete = (row) => {
+console.log(state);
+
+const getState = () => state;
+
+const getWord = () => state.word;
+const setWord = (word) => state.word = word;
+
+const getAttempts = () => state.attempts;
+const addAttempt = () => state.attempts = state.attempts + 1;
+
+const getIsSolved = () => state.isSolved;
+const setIsSolved = (isSolved) => state.isSolved = isSolved; 
+
+const getCurrentRow = () => state.currentRow;
+const setCurrentRow = (row) => state.currentRow = document.getElementById(row);
+
+const getCurrentWord = () => state.currentWord;
+const setCurrentWord = (currentWord) => state.currentWord = currentWord;
+
+const setCellListeners = (currentRow) => {
+    const cells = currentRow.querySelectorAll('.cell');
+
+    cells[0].focus();
+
+    cells.forEach((cell, idx) => {
+        cell.addEventListener('keydown', ({key}) => {
+
+            if(key >= 'a' && key <= 'z') {
+                cell.value = '';
+                if(idx < cells.length - 1) {
+                    setTimeout(() => cells[idx + 1].focus(), 10);
+                }
+            }else if(key === 'Enter' || key === 'Return') {
+                handleEnter();
+
+            }else if (key === 'Backspace'){
+
+                if(idx > 0) {
+                    setTimeout(() => {
+                        cells[idx - 1].focus(); 
+                        cell.value = '';
+                    }, 10);
+                }
+            } else if(key === 'ArrowRight') {
+                idx < cells.length - 1 && cells[idx + 1].focus(); 
+            } else if (key === 'ArrowLeft') {
+                idx > 0 && cells[idx - 1].focus(); 
+            }
+            else {
+                console.log(key, 'Invalid!'); 
+                setTimeout(() => {
+                    cell.value = '';
+                    cells[idx].focus(); 
+                }, 0);
+                
+            }
+        })
+    }); 
+}
+
+const handleEnter = () => {
+    
+}
+
+const checkRowComplete = (currentRow) => {
     let count = 0;
-    row.forEach(cell => cell.value !== '' && count++);
+    currentRow.forEach(cell => cell.value !== '' && count++);
     return count === 5 ? true : false;
 }
 
-const updateCurrentWord = (row) => {
+const updateCurrentWord = (currentRow) => {
     const word = []; 
-    row.forEach(cell => word.push(cell.value));
+    currentRow.forEach(cell => word.push(cell.value));
     return word;
 }
 
-const row = document.querySelectorAll('.cell');
-row[0].focus();
 
-row.forEach((code, idx) => {
-  code.addEventListener('keydown', (event) => {
-    if (event.key >= 'a' && event.key <= 'z') {
-      row[idx].value = '';
+/* **************************** */
 
-      if(idx < row.length - 1) {
-        setTimeout(() => row[idx + 1].focus(), 10);
-      }
-      
-    } else if (event.key === 'Backspace' && idx !== 0) {
-      setTimeout(() => row[idx - 1].focus(), 10);
-    } 
-  });
-});
+setCellListeners(getCurrentRow());
 
-document.addEventListener('keydown',(event) => {
-    if(event.key === 'Enter' || event.key === 'Return') {
-        console.log('Enter was pressed!');
-        console.log(checkRowComplete(row));
-        console.log(updateCurrentWord(row));
-    }
-});
+
 
